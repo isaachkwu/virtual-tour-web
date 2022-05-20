24f491e4904ae7c55ec19baaf1267f8a42459097
Isaac Wu <isaachkwu@gmaill.com>
2022-05-20 17:00:05 +0800

    add nav button, vistacontainer, edit config

--- next.config.js
+++ next.config.js
@@ -3,10 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-// module.exports = nextConfig
-module.exports = {
-  webpack(config) {
-    config.infrastructureLogging = { debug: /PackFileCache/ }
-    return config;
-  }
-}
+module.exports = nextConfig
+// module.exports = {
+//   webpack(config) {
+//     config.infrastructureLogging = { debug: /PackFileCache/ }
+//     return config;
+//   }
+// }
